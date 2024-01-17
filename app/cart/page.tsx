2f74b6cf86a797bc9317e6a83fb6413fd76a5802
S Deepak Kumar <dipkfilms@gmail.com>
2024-01-17 16:22:36 +0530

    Cart Page Added

---
+++ app/cart/page.tsx
@@ -0,0 +1,186 @@
+"use client";
+
+import {
+  XMarkIcon as XMarkIconMini,
+} from "@heroicons/react/20/solid";
+
+const products = [
+  {
+    id: 1,
+    name: "Basic Tee",
+    href: "#",
+    price: "$32.00",
+    color: "Sienna",
+    inStock: true,
+    size: "Large",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in sienna.",
+  },
+  {
+    id: 2,
+    name: "Basic Tee",
+    href: "#",
+    price: "$32.00",
+    color: "Black",
+    inStock: false,
+    leadTime: "3–4 weeks",
+    size: "Large",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-02.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+  },
+  {
+    id: 3,
+    name: "Nomad Tumbler",
+    href: "#",
+    price: "$35.00",
+    color: "White",
+    inStock: true,
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-03.jpg",
+    imageAlt: "Insulated bottle with white base and black snap lid.",
+  },
+];
+
+export default function Cart() {
+
+  return (
+    <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
+      <span className="text-gray-400 text-sm font-[300]">Home</span>
+      <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+        / Shopping Cart
+      </h1>
+
+      <form className="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
+        <section aria-labelledby="cart-heading" className="lg:col-span-7">
+          <h2 id="cart-heading" className="sr-only">
+            Items in your shopping cart
+          </h2>
+
+          <ul
+            role="list"
+            className="divide-y divide-gray-200 border-b border-t border-gray-200"
+          >
+            {products.map((product, productIdx) => (
+              <li key={product.id} className="flex py-2 sm:py-4">
+                <div className="flex-shrink-0">
+                  <img
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    className="h-12 w-12 rounded object-cover object-center sm:h-16 sm:w-16 bg-gray-200"
+                  />
+                </div>
+
+                <div className="ml-4 flex flex-1 flex-col justify-between sm:ml-6">
+                  <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
+                    <div>
+                      <div className="flex justify-between">
+                        <h3 className="text-sm">
+                          <a
+                            href={product.href}
+                            className="font-medium text-gray-700 hover:text-gray-800"
+                          >
+                            {product.name}
+                          </a>
+                        </h3>
+                      </div>
+                      <div className="mt-1 flex text-sm">
+                        <p className="text-gray-500">{product.color}</p>
+                        {product.size ? (
+                          <p className="ml-4 border-l border-gray-200 pl-4 text-gray-500">
+                            {product.size}
+                          </p>
+                        ) : null}
+                      </div>
+                      <p className="mt-1 text-sm font-medium text-gray-900">
+                        {product.price}
+                      </p>
+                    </div>
+
+                    <div className="mt-4 sm:mt-0 sm:pr-9">
+                      <label
+                        htmlFor={`quantity-${productIdx}`}
+                        className="sr-only"
+                      >
+                        Quantity, {product.name}
+                      </label>
+                      <select
+                        id={`quantity-${productIdx}`}
+                        name={`quantity-${productIdx}`}
+                        className="max-w-full rounded border border-gray-300 py-1.5 px-2 text-left text-base font-medium leading-5 text-gray-700 sm:text-sm"
+                      >
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                        <option value={6}>6</option>
+                        <option value={7}>7</option>
+                        <option value={8}>8</option>
+                      </select>
+
+                      <div className="absolute right-0 top-0">
+                        <button
+                          type="button"
+                          className="-m-2 inline-flex p-2 text-gray-400 hover:text-gray-500"
+                        >
+                          <span className="sr-only">Remove</span>
+                          <XMarkIconMini
+                            className="h-5 w-5"
+                            aria-hidden="true"
+                          />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        {/* Order summary */}
+        <section
+          aria-labelledby="summary-heading"
+          className="mt-16 rounded border-[1.5px] border-black px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
+        >
+          <h2
+            id="summary-heading"
+            className="text-xl font-medium text-gray-900"
+          >
+            Cart Total
+          </h2>
+
+          <dl className="mt-6 space-y-4">
+            <div className="flex items-center justify-between">
+              <dt className="text-sm text-gray-600">Subtotal</dt>
+              <dd className="text-sm font-medium text-gray-900">$99.00</dd>
+            </div>
+            <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+              <dt className="flex items-center text-sm text-gray-600">
+                <span>Shipping Charge</span>
+              </dt>
+              <dd className="text-sm font-medium text-gray-900">Free</dd>
+            </div>
+            <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+              <dt className="text-base font-medium text-gray-900">
+                Total
+              </dt>
+              <dd className="text-base font-medium text-gray-900">$112.32</dd>
+            </div>
+          </dl>
+
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="w-full rounded border border-transparent bg-[#DB4444] px-4 py-3 text-base font-medium text-white text-sm"
+            >
+              Checkout
+            </button>
+          </div>
+        </section>
+      </form>
+    </div>
+  );
+}
